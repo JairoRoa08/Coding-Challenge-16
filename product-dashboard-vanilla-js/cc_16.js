35b4ea582d2fa4ef11f518b98d9160b6c9f5dc6f
Jairo Roa <Jroa@usf.edu>
2025-03-30 19:49:28 -0400

    Display products in the DOM with name, price, and image

--- product-dashboard-vanilla-js/cc_16.js
+++ product-dashboard-vanilla-js/cc_16.js
@@ -1,3 +1,4 @@
+// Task 2:  Fetch Products with .then()
 function fetchProductsThen() {
     fetch("https://www.course-api.com/javascript-store-products")
         .then(response => {
@@ -12,3 +13,28 @@ function fetchProductsThen() {
         .catch(error => console.error("Fetch error:", error)); // Catches Errors
 }
 fetchProductsThen();
+// Task 3: Fetch Products with async/await
+async function fetchProductsAsync() {
+    try {
+        const response = await fetch("https://www.course-api.com/javascript-store-products");
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const products = await response.json();
+        displayProducts(products);
+    } catch (error) {
+        handleError(error);
+    }
+}
+function displayProducts(products) {
+    const container = document.getElementById("products-container");// Display Products
+    if (!container) return;
+
+    container.innerHTML = products
+        .map(product => `<p>${product.fields.name}</p>`)
+        .join("");
+}
+function handleError(error) {
+    console.error("Fetch error:", error); // Handles Error
+}
+fetchProductsAsync();
